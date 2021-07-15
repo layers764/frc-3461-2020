0ff55aa7001acad55d8012f78dcb79650aeda2aa
layers764 <51806658+layers764@users.noreply.github.com>
2021-07-15 12:21:55 -0400

    Install Dependencies that were missing

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -9,7 +9,9 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANEncoder;
 
-import com.revrobotics.CANPIDController;
+
+
+robotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
